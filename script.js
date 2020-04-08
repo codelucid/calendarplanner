1a168663bded048c2f8e6ffa5f8452ab65bdb0c0
the roc <61427314+codelucid@users.noreply.github.com>
2020-04-08 13:33:15 -0400

    update to html with jquery question

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 $(document).ready(function(){
+    
+    $('currentDay').text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+   
 
     $('.saveBtn').click(function(){
         alert('btn save is clicked')
     });
 
-    
+    var newDiv = $("<div></div>").newDiv("")
 
 
 });
