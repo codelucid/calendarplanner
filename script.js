8764854d89047c4ae514b106bd8006ea5efc88fc
the roc <61427314+codelucid@users.noreply.github.com>
2020-04-11 16:05:09 -0400

    update with delete button and final touches

--- script.js
+++ script.js
@@ -3,15 +3,16 @@
 
 
 var hour = moment().hours();
-console.log(hour);
-
-$(document).ready(function(){
-    
-    function getDate(){
+console.log(hour);   
+ function getDate(){
         $('#currentDay').text(moment().format('MMMM Do YYYY, h:mm:ss a'));
     
     };
+
+$(document).ready(function(){
     
+
+    getDate();
     
     setInterval(getDate,1000);
 
@@ -24,12 +25,24 @@ $(document).ready(function(){
         localStorage.setItem(locationId, value);
     });
 
-    $(".description").each(function(){
-        var id = $(this).parent().attr("id");
-        $(this).val(localStorage.getItem(id));
-    })
+    
+    $('.clearBtn').click(function(){
+      
+        var value = $(this).siblings(".description").val();
+        var locationId = $(this).parent().attr("id");
+
+        localStorage.removeItem(locationId);
+        location.reload();
+    });
+
+
 
     $(".description").each(function(){
+        //get storage
+        var id = $(this).parent().attr("id");
+        $(this).val(localStorage.getItem(id));
+
+        //change color
         var rowHour = $(this).parent().attr("id")
         var rowNumber = parseInt(rowHour);
         console.log(rowNumber);
@@ -42,6 +55,7 @@ $(document).ready(function(){
         };
     
     });
+
     
 });
 
